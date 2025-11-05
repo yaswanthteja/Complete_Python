6ca468d1e4399a3cdb804aab8d1c7c9924fdfaf0
yaswanthteja <yaswanthteja02@gmail.com>
2025-11-05 13:21:05 +0530

    Added dicitionary methods

---
+++ 04_Data_Structures/04_Dictionary/capitals.py
@@ -0,0 +1,8 @@
+# dictionary =a collecction of {key:value} pairs ordered and Changeable . no duplicates (keys)
+
+capitals={
+    "USA":"washington D.C",
+    "India":"New Delhi",
+    "China": "Beijing",
+    "Russia" :"Moscow"
+}
